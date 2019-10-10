cf5a824ab11a2912287cd03346fb3d9e9fbde09f
Gregori Spielmann <gregorispielmann@MacBook-Pro-de-Gregori.local>
2019-10-10 18:52:26 -0300

    yarn lock update

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 
-import promise from 'redux-promise'
-import multi from 'redux-multi'
-import thunk from 'redux-thunk'
+import promise from "redux-promise";
+import multi from "redux-multi";
+import thunk from "redux-thunk";
 
-import App from './main/App'
-import reducers from './main/reducers'
+import App from "./main/App";
+import reducers from "./main/reducers";
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 // const store = createStore(reducers, devTools)
-const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers, devTools)
+const store = applyMiddleware(thunk, multi, promise)(createStore)(
+  reducers,
+  devTools
+);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App></App>
-    </Provider>
-    , document.getElementById('root'));
-
+  <Provider store={store}>
+    <App></App>
+  </Provider>,
+  document.getElementById("root")
+);
